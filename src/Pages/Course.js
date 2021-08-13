57722d499b971203dc3946c4ca23241ba19325bc
Mark <mark.nelubin@gmail.com>
2021-08-13 21:28:43 +0300

    small styling fixes

--- src/Pages/Course.js
+++ src/Pages/Course.js
@@ -9,10 +9,10 @@ const Course = () => {
         <div className="py-5">
             {courses.map((course, index) =>
                 <Container className="py-4" key={index}>
-                    <div class="p-5 mb-4 bg-light rounded-3">
-                        <div class="container-fluid py-5 ">
-                            <h1 class="display-5 fw-bold">{course.courseTitle}</h1>
-                            <p class="col-md-12 fs-4">{course.courseDescription}</p>
+                    <div className="p-sm-3 p-lg-5 mb-4 bg-light rounded-3" id="course-jumbo">
+                        <div className="container-fluid py-5 ">
+                            <h1 className="display-5 fw-bold">{course.courseTitle}</h1>
+                            <p className="col-md-12 fs-5">{course.courseDescription}</p>
                             <Link className="btn btn-primary btn-lg" to={`/course/${course.courseID}`}>Изучить</Link>
                         </div>
                     </div>
